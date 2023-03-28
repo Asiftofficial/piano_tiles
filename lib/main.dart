9720fcebaceba68761541cd0d83e0d672629f4ab
Anas <aswaranas025@gmail.com>
2023-03-28 22:38:19 +0700

    add some feature

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Piano Tiles test',
+      title: 'Piano Tiles test feature',
       home: MovingContainer(),
     );
   }
